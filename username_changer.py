d29f173eb1cfd73953c15b7ebc7964dcef1cf987
Draguve <ritwikpatil1998@gmail.com>
2018-06-12 23:44:56 +0530

    Added settings for ini's and inital reading function

--- username_changer.py
+++ username_changer.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 import re
-from shutil import copyfile 
+from shutil import copyfile
+from configparser import ConfigParser
 
 allowed_names = ["SmartSteamEmu.ini","steam_emu.ini","ALI213.ini","Origins.ini","steam_api.ini","stp-steam.ini","rev.ini","CPY.ini"]
+ini_settings = {"SmartSteamEmu":"PersonaName ","Settings":"UserName","Settings":"PlayerName","UserName":"Name","Globals":"PersonaName","steamclient":"PlayerName"}
 
 def find_files(directory):
     files = []
@@ -21,3 +23,8 @@ def backup_files(file_paths):
     for win_path in file_paths:
         path = str(win_path)
         copyfile(path,path+".bak")
+
+def rename_fields(path):
+    config = ConfigParser()
+    config.read(path)
+    return config
