592a67011a1653a5e3f87193b3dba4005d497d9c
draguve <draguve@gmail.com>
2018-06-12 23:00:54 +0530

    Added check for filenames and gitignore fix

--- username_changer.py
+++ username_changer.py
@@ -1,10 +1,13 @@
 from pathlib import Path
 import re
 
+allowed_names = ["SmartSteamEmu.ini","steam_emu.ini","ALI213.ini","Origins.ini","steam_api.ini","stp-steam.ini","rev.ini","CPY.ini"]
+
 def find_files(directory):
     pathlist = Path(directory).glob('**/*.ini')
     for path in pathlist:
-        print(extract_basename(path))
+        if (extract_basename(path)) in allowed_names:
+            print(path)
 
 def extract_basename(path):
     basename = re.search(r'[^\\/]+(?=[\\/]?$)', str(path))
